5ec0a63286b18712ae581b1930307cf871f1ad1e
dldbstj22 <144695255+dldbstj22@users.noreply.github.com>
2025-11-11 13:55:57 +0900

    프롬프트 버전  업데이트

--- src/components/prompts.js
+++ src/components/prompts.js
@@ -1,7 +1,7 @@
 export const PROMPTS = {
     opening:    { id: "pmpt_68c122b7fbc88196bdc8680f93b044860281f0ca8bc85937", version: "20" },
     dilemma: { id: "pmpt_68c123df86948197a9be12f7344438270a5924971aab8f27", version: "15" },
-    flip:    { id: "pmpt_68c1294a9c788190a354084edf3e5b4e0551d09aeaea88c0", version: "19" },
+    flip:    { id: "pmpt_68c1294a9c788190a354084edf3e5b4e0551d09aeaea88c0", version: "21" },
     roles:    { id: "pmpt_68c12508f4088193905744bdf4a5aa4a0b87855a9b1545b0", version: "15" },
     ending:   { id: "pmpt_68c12982ae048190aaed3a4004918f1a09e33c363c985ff5", version: "20" }
   };
